de2563e26fa6c104c91725eb6500a00c768d2f98
Teyssier Stephane <stephane.teyssier@wescale.fr>
2018-07-05 17:50:35 +0200

    Remove gitlab module

--- modules/terraform-ecs-cluster-module/cloudwatch_logs.tf
+++ modules/terraform-ecs-cluster-module/cloudwatch_logs.tf
@@ -1,9 +1,3 @@
-module "ec2_logs" {
-  source       = "git::ssh://git@gitlab.socrate.vsct.fr/terraformcentral/terraform-ec2-common-cwlog-module.git?ref=v1.0.2"
-  component_id = "${local.component_id}"
-  env          = "${var.env}"
-}
-
 resource "aws_cloudwatch_log_group" "ecs_agent_log" {
   name              = "/ec2/${var.env}/${local.component_id}/ecs/ecs-agent.log"
   retention_in_days = 14
